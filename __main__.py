c17c97d3ab6af19c59a298f40ebdf25273e1155b
fernando-churros <fernando_churros@outlook.com>
2026-09-01 04:39:43 -0300

    feat(cli): implementar menu interativo e integracao com operacoes do condominio
        - Adiciona laco interativo com match/case para cadastro, remocao, busca e encerramento
        - Integra fluxo de operacoes com tratamento centralizado de exceções

--- __main__.py
+++ __main__.py
@@ -1,11 +1,44 @@
 from Morador import Morador
+from Condominio import Condominio
 
 def main():
-    try:
-        m1 = Morador('fernando', '102', '41987410667')
-        print(m1.__dict__)
-    except (ValueError, TypeError) as erro:
-        print(erro)
+    condominio = Condominio()
+    condominio.carregar_moradores()
+
+    while True:
+        try:
+            option = int(input('1 - Cadastrar 2 - Remover 3 - Buscar 5 - Sair\n: ')) 
+
+            match option:
+                case 1:
+                    nome = str(input('Nome: '))
+                    apto = str(input('Apto: '))
+                    telefone = str(input('telefone: '))
+
+                    condominio.adicionar(Morador(nome, apto, telefone))
+                    print('Morador cadastrado com sucesso!')
+                case 2:
+                    apto = str(input('Apto: '))
+                    condominio.remover(apto)
+                    print('Morador excluido com sucesso!')
+                case 3:
+                    apto = str(input('Apto: '))
+
+                    morador = condominio.buscar_apto(apto)
+                    if not morador:
+                        print('Morador não encontrado')
+                    else:
+                        print('-' * 30)
+                        print(morador)
+                        print('-' * 30)
+
+                case 5:
+                    print('\nSaindo...\n')
+                    break
+
+        except (ValueError, TypeError) as erro:
+            print(erro)
+
 if __name__ == '__main__':
     main()
 
